e86ea02edbfa85e46745cccb981c3ea27965a46d
kevQL <unajmakev@gmail.com>
2019-11-04 22:06:03 -0500

    Se ordenó el código en rxd y rxp

--- reportes/rxp.php
+++ reportes/rxp.php
@@ -47,69 +47,86 @@ class PDF extends FPDF
 
         $pdf->SetFont('Arial','B',8);
         $pdf->SetXY(21,4.5);
-        $pdf->Cell(100,10,"REGISTRO VACIO PARA {$mesLit} del {$anio}",0,0,'C');
+        $pdf->Cell(100,10,"SIN REGISTRO PARA {$mesLit} del {$anio}",0,0,'C');
     }else{
         $dataQuery = $arrData['data'];
-        header('Content-Type: text/html; charset=UTF-8');
+
         while($element = $dataQuery->fetch(PDO::FETCH_ASSOC)){   
 
-            $esta_cancelado = $element['esta_cancelado']?'RECIBO CANCELADO':'FALTA PAGAR RECIBO';
-            
-            
-        //verifica que sea una institución para solo imprimir el nombre        
-        $_POST['urlimg'] = $element['estado_corte']?'img/corte.jpg':'img/reciboAgua.jpg';
+            $esta_cancelado = $element['esta_cancelado']?'RECIBO CANCELADO':'FALTA PAGAR RECIBO';                    
+            //verifica que sea una institución para solo imprimir el nombre        
+            $_POST['urlimg'] = $element['estado_corte']?'img/corte.jpg':'img/reciboAgua.jpg';
 
-        //recuperar los meses de las deudas anteriores
-        $deudasMes = $dataObj->consultaDeudasMes($element['cod_suministro']);
-
+            //recuperar los meses de las deudas anteriores
+            $deudasMes = $dataObj->consultaDeudasMes($element['cod_suministro']);
 
             $medidor = $element['tiene_medidor']?'Si':'No';    
             $cortado = $element['estado_corte']?'Si':'No';
             $nombre_completo = $element['categoria_suministro']=='Estatal'?utf8_decode($element['nombre']):utf8_decode($element['apellido']." ".$element['nombre']); 
-            $msjCorte = ($element['contador_deuda']>=2)?'EN CORTE':'no está en corte';
+            $msjCorte = ($element['contador_deuda']>=3)?'EN CORTE':'no está en corte';
 
+            //AGREGAMOS PÁGINA ****************************************************
             $pdf->AddPage();    
             $pdf->Image($_POST['urlimg'] ,0,0,148,210);
-            
-            $pdf->SetFont('Arial','B',20);
-            $pdf->SetXY(5,7);
-            $pdf->Cell(30,12,'ASUSAP',0,0,'C');
 
-            $pdf->SetFont('Arial','B',7);        
-            //fecha seleccionado para el recibo 
-            $pdf->SetXY(5,12);
-            $pdf->Cell(30,12,"{$mesLit} del {$anio}",0,0,'C');
-
-            $pdf->SetXY(57,7);
-            $pdf->Cell(60,5,"$nombre_completo",0,0,'');//
-            $pdf->SetXY(57,8);
-            $pdf->Cell(60,10,$element['direccion'],0,0,'');
+        //código del suministro
+        $pdf->SetFont('Arial','B',15);     
+        $pdf->SetXY(103,5);
+        $pdf->Cell(45,8,$element['cod_suministro'],0,0,'C');
         
-            $pdf->SetXY(27,95);
-            $pdf->Cell(100,10,$esta_cancelado,0,0,'C');
-            $pdf->SetXY(27,100);
-            $pdf->Cell(100,10,$element['monto_pagar'],0,0,'C');
-            $pdf->SetXY(27,105);
-            $pdf->Cell(100,10,$element['cod_suministro'],0,0,'C');
-            $pdf->SetXY(27,110);
-            $pdf->Cell(100,10,$element['categoria_suministro'],0,0,'C');
-            $pdf->SetXY(27,115);
-            $pdf->Cell(100,10,"Tiene medidor:".$medidor,0,0,'C');
-            $pdf->SetXY(10,99);
-            $pdf->Cell(100,5,"Cantidad deudas: {$element['contador_deuda']}",0,0,'');
+        //CONFIGURANDO EL TAMAÑO Y TIPO DE LETRA
+        $pdf->SetFont('Arial','B',7);  
 
+        //PRIMERA COLUMNA - INFORMACIÓN GENERAL ****************************************************
+        $pdf->SetXY(21,35);
+        $pdf->Cell(50,5,$nombre_completo ,0,0,'');  //nombre de tituar
+        $pdf->SetXY(24,40.5);
+        $pdf->Cell(50,5,$element['direccion'],0,0,'');    //direccion del titular 
+        $pdf->SetXY(8,43);
+        $pdf->Cell(100,10,utf8_decode("SAN JERÓNIMO"),0,0,''); //distrito
+        $pdf->SetXY(11,48.3);
+        $pdf->Cell(100,10,$element['categoria_suministro'],0,0,''); //categoria del suministro
+
+        //SEGUNDA COLUMNA - INFORMACIÓN DE PAGO ****************************************************
+        $pdf->SetXY(118,35);
+        $pdf->Cell(50,5,"{$mesLit} del {$anio}",0,0,''); //mes facturado 
+        $pdf->SetXY(129,40.3);
+        $pdf->Cell(50,5,"mes",0,0,''); //frecuencia de facturación
+        $pdf->SetXY(121,45.5);
+        $pdf->Cell(50,5,$element['fecha_emision'],0,0,''); //fecha emision
+        $pdf->SetXY(125,50.7);
+        $pdf->Cell(50,5,$element['fecha_vencimiento'],0,0,''); //fecha vencimiento
+
+
+        //REGISTROS DEL MEDIDOR ****************************************************
+        $pdf->SetXY(7,62);
+        $pdf->Cell(100,10,$medidor,0,0,''); // Tiene medidor??
+
+
+        //INFORMACIÓN COMPLEMENTARIA ****************************************************              
+        $pdf->SetXY(10,75);
+        $pdf->Cell(100,10,$esta_cancelado,0,0,''); // Está cancelado el recibo ??
+        
+        $pdf->SetXY(10,82);
+        $pdf->Cell(100,5,"Cantidad deudas: {$element['contador_deuda']}",0,0,'');   
+        
         //Imprime los meses endeudados
-        $pdf->SetXY(10,104);     
+        $pdf->SetXY(10,87);        
         foreach ($deudasMes as $value) {
             # code...
             $nombreMes =  $dataObj->obtenerNombrefecha($value['anio'],$value['mes']);
             $pdf->SetX(10);
             $pdf->Cell(30,3,$nombreMes['r_mes']." del ".$nombreMes['r_anio'],1,0,'');
             $pdf->ln();
-        }   
-
-            $pdf->SetXY(10,150);
-            $pdf->Cell(0,10,utf8_decode($msjCorte),0,0,'');
+        }  
+        
+        //DETALLE DE LA FACTURACIÓN ****************************************************
+        $pdf->SetXY(118,139);
+        $pdf->Cell(100,10,"S/ ".$element['monto_pagar'],0,0,'');
+        
+        //mensaje de corte
+        $pdf->SetXY(10,150);
+        $pdf->Cell(0,10,utf8_decode($msjCorte),0,0,'');
 
         }        
     }
