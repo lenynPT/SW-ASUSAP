f493652634f0f973fc4ac5a51bed1ef2cd06eb58
kevQL <unajmakev@gmail.com>
2019-11-22 16:23:20 -0500

    Corriendo error en gconsumo para habilitar consumo

--- views/modules/gconsumoL.php
+++ views/modules/gconsumoL.php
@@ -1,11 +1,23 @@
 
+<style>
+    .btn-init-consumo{
+        text-transform:uppercase;
+        background-color:rgba(220,151,10,.5);
+        padding:20px!important;
+        font-size:1.5em!important;
+    }
+</style>
 <!-- Este btn actualiza la tabla de los meses y años en la tabla 'estado_gconsumo'-->
 <?php 
     if($btn_habilitarConsumo){    
 ?>
-    <button id="btn_updGC" class="btn btn-danger btn-lg btn-block p-5 ">
-        HABILITAR CONSUMO PARA MES <?php echo "$fecha_btn_habCons del año $anio_btn_habCons"; ?>
+    <button id="btn_updGC" class="btn-init-consumo btn btn-danger btn-lg btn-block p-5">
+        HABILITAR CONSUMO PARA EL MES DE <b><?php echo "$fecha_btn_habCons del año $anio_btn_habCons"; ?></b>
     </button>   
-<?php } ?>
+<?php }else{
+    echo "<br><br><br>";
+} ?>
 
-<h1 class="text-center"> YA SE GENERÓ EL CONSUMO PARA EL MES <?php echo "$fechaGenerado del año $anioGenerado";?></h1>
+<h3 class="text-center lead"> 
+    SE GENERÓ LOS CONSUMOS PARA <br> <b><?php echo "$fechaGenerado Del $anioGenerado";?> </b>
+</h3>
